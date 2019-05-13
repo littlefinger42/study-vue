296e02aeed5cf5c1e5c88b68a60a088d158fd731
littlefinger42 <lauriemail@protonmail.com>
2019-05-13 18:05:12 +0200

    Added request for bluetooth device

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '@/components/Home'
-import Settings from '@/components/Settings'
+import Home from '@/views/Home'
+import Settings from '@/views/Settings'
 
 Vue.use(Router)
 
---
+++ src/views/Settings.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <v-switch
+      v-model="switch1"
+      v-on:click="requestBluetooth"
+      :label="`Enable bluetooth control: ${switch1.toString()}`"
+    ></v-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  data: function() {
+    return {
+      msg: "Settings",
+      switch1: false
+    };
+  },
+  methods: {
+    requestBluetooth: () => {
+      navigator.bluetooth
+        .requestDevice({ filters: [{ namePrefix: ["NAMEPREFIX"] }], optionalServices: [0xFFE0] })
+        // .then(device => {
+        //   /* ... */
+        // })
+        // .catch(error => {
+        //   console.log(error);
+        // });
+    }
+  }
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
