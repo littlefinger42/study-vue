119e0d02b1a316a1d13193cc696e44b8cc87be2e
littlefinger42 <lauriemail@protonmail.com>
2019-05-13 16:52:34 +0200

    Implemented vue-routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">Link to home</router-link>
+    <router-link to="/settings">Link to settings</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // Home
   }
 }
 </script>
